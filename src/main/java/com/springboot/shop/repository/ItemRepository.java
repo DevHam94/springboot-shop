2813ecb281f059b5ea43759106f6a7d7938c0c76
gkaeo <gkaeogus7@gmail.com>
2025-03-18 09:32:52 +0900

    250318

--- src/main/java/com/springboot/shop/repository/ItemRepository.java
+++ src/main/java/com/springboot/shop/repository/ItemRepository.java
@@ -2,6 +2,8 @@ package com.springboot.shop.repository;
 
 import com.springboot.shop.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -12,4 +14,7 @@ public interface ItemRepository extends JpaRepository<Item, Long> {
     List<Item> findByItemNmOrItemDetail(String itemNm, String itemDetail);
 
     List<Item> findByPriceLessThan(Integer price);
+
+    @Query("select i from Item i where i.itemDetail like %:itemDetail% order by i.price desc")
+    List<Item> findByItemDetail(@Param("itemDetail") String itemDetail);
 }
